4bf1106e485f9e4c4729cf837e85d407251215a1
vijay kumar <308vijaykumar@gmail.com>
2023-07-01 19:58:53 +0530

    user profile menu

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    
+    <ul class="navbar_icons">
+        <li> <i class="fa-solid fa-s olid fa-bell"></i>  </li>
+
+        <li class="dropdown">
+            <i class="fa-solid fa-circle-user" id="userAccount"></i>
+            <ul class="user_profile dropdown-content" id="userProfile">
+                <a href="#"><li>View Profile</li></a>
+                <a href="#"><li>change password</li></a>
+                <a href="#"><li>Logout</li></a>
+            </ul>
+        </li>
+    </ul>
 </header>
